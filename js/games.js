d1eb71f451c119f863afd0aa831629c5098599f0
Herschel_TC <raphael.lesterlin@gmail.com>
2025-05-20 14:43:35 +0200

    Finished first inspect element game and final disappearance

--- js/games.js
+++ js/games.js
@@ -5,12 +5,49 @@ var complete = 0;
 window.addEventListener("load", function() { //When page fully loaded, puts only html to start the game
     document.getElementById("inspect").style.display = "none";
     document.getElementById("init").style.display = "flex"; //Makes initial stuff appear, disappears other sections
+
     document.getElementById("ready").addEventListener("click", function() { //button triggers game start
+
         console.log("Game start");
         document.getElementById("init").classList.add("bye-bye"); //Adds init section to bye bye which triggers fading out animation
+
         setTimeout(function() { //After animation execution replaces with game
+
             document.getElementById("init").style.display = "none";
             document.getElementById("inspect").style.display = "flex";
+
+            document.getElementById("inspect_f").addEventListener("click", function() { //Event listener for the second game
+
+                const answ2 = document.getElementById("2text").value; //Retrieves textbox value
+
+                var good_answer = document.createElement("p"); //Creates answer elements, whether the player is right or wrong
+                good_answer.style.color = "green";
+                good_answer.textContent = "Correct ! This is a hex code for the specie Vaquita, a small mexican cetacean. Only 10 individuals remains in the wild sadly. Let's move on...";
+                good_answer.style.margin = "auto";
+                var wrong_answer = document.createElement("p");
+                wrong_answer.style.color = "red";
+                wrong_answer.textContent = "Wrong ! Search more in depth. Hint : look out with inspect element...";
+                wrong_answer.style.margin = "auto";
+
+                if (answ2 === '0x76 0x61 0x71 0x75 0x69 0x74 0x61') {
+                    document.querySelector("#inspect > div").appendChild(good_answer);
+                    document.querySelector("#inspect > div").style.left = "17%";
+                    setTimeout(function() {
+                        setTimeout(function() {
+                            document.querySelector("#inspect").style.display = "none";
+                        } ,1800)
+                        document.querySelector("#inspect").classList.add("bye-bye");
+                    }, 2200); //timeout to let the player long enough time to read, longer here since longer answer
+                } else {
+                    document.querySelector("#inspect > div").appendChild(wrong_answer);
+                    setTimeout(function() {
+                        setTimeout(function() {
+                            document.querySelector("#inspect > div").removeChild(wrong_answer);
+                        } ,1800)
+                        wrong_answer.classList.add("bye-bye");
+                    }, 1800); //timeout to let the player long enough time to read
+                }
+            });
         }, 1800);
     });
 })
