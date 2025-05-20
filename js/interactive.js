09320d3122a23907906a4c9db02adcbd094d510c
Nightfury <goldfury.exe@gmail.com>
2025-05-20 15:56:40 +0200

    starting to cook something in js for stats.html

---
+++ js/interactive.js
@@ -0,0 +1,7 @@
+var stats = [220, 100, 46, 500, 97];
+var facts = [" million tons of plastic waste has been created in 2024", " million marine animals die each year from plastic waste alone", "% of the plastic items we create is in the ocean", " 'dead ocean zones' across the world, covering a zone greater than the UK", "% of the ocean's surface is unprotected from fishing"];
+var sources = ["https://www.condorferries.co.uk/marine-ocean-pollution-statistics-facts", "https://reef-world.org/blog/sad-facts"];
+
+function displayOnLoad() {
+    console.log("iuhiern");
+}
