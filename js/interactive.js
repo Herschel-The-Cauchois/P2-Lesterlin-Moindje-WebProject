1dbc6ad5f649a25c9e36415cdf7d8e40491d3ad1
Nightfury <goldfury.exe@gmail.com>
2025-05-21 22:38:03 +0200

    finished coding said extremely cool thingy in stats.html, we just need to add mroe statistics to increase the pool and maybe fill the bottom of the page

--- js/interactive.js
+++ js/interactive.js
@@ -3,5 +3,21 @@ var facts = [" million tons of plastic waste has been created in 2024", " millio
 var sources = ["https://www.condorferries.co.uk/marine-ocean-pollution-statistics-facts", "https://reef-world.org/blog/sad-facts"];
 
 function displayOnLoad() {
-    console.log("iuhiern");
+    var factno = Math.floor(Math.random() * facts.length);
+    var headtext = document.querySelector("#headstat > p");
+    headtext.style.fontSize = "250px";
+
+    var timer = 100;
+    var numberAnimation = 0;
+    var interval = setInterval(function() {
+        timer--;
+        console.log(timer);
+        numberAnimation += stats[factno]/100;
+        headtext.textContent = Math.floor(numberAnimation);
+        if (timer == 0) {
+            headtext.textContent = stats[factno];
+            document.getElementById("headstattext").textContent = facts[factno];
+            clearInterval(interval);
+        }
+    }, 10);
 }
